f804b37031429096093e07d67bee5ffb684298fe
Stalev02 <yuri.stalev@mail.ru>
2022-03-02 20:52:35 +0300

    oh yeah

---
+++ ProductAtShop/src/ru/inordic/lesson5/Product.java
@@ -0,0 +1,8 @@
+package ru.inordic.lesson5;
+
+import java.util.Scanner;
+
+public class Product {
+    private int cost;
+
+}
---
+++ ProductAtShop/src/ru/inordic/lesson5/productsAtShop.java
@@ -0,0 +1,15 @@
+package ru.inordic.lesson5;
+
+import java.util.Scanner;
+
+public class productsAtShop {
+    public static void main(String[] args) {
+        Product potato = new Product();
+        potato.setCost(34);
+        System.out.println(potato.cost);
+
+
+    }
+}
+
+
